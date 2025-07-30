42532a4a5f8829da9972f3b8e861a01a35098910
Diana144-hue <Dianachepngeno251@gmail.com.>
2025-07-30 15:59:21 +0300

    Update templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,13 +8,12 @@ from django.views.generic import DetailView
 # function-based view
 def list_books(request):
     books = Book.objects.all()
-    response = "\n".join([f"{book.title} by {book.author}" for book in books])
-    return HttpResponse(response, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
